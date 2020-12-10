f07854ced380a6e7dc2952bae0fb2f76806df5f5
Tom Golden <tbjgolden@gmail.com>
2020-12-09 20:22:25 -0600

    working w/ text

--- www/index.js
+++ www/index.js
@@ -1,3 +1,13 @@
-import * as wasm from "game-of-life-rust-wasm";
+import { Universe } from "game-of-life-rust-wasm";
 
-wasm.greet();
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
